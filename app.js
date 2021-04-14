8540f6ea1098424845e7e26040af08fe46e9dbb8
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-14 14:04:41 +0100

    apply graphql middleware to express

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
+
+// middleware to tell express to use graphql
+app.use("/graphql", graphqlHTTP({
+    
+}));
 
 app.listen(4000, () => {
     console.log("server started on localhost:4000");
