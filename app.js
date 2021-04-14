7b89429641bdfdde9e8da729372d8778e2a37be9
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-14 14:28:06 +0100

    import schema into app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 // middleware to tell express to use graphql
 app.use("/graphql", graphqlHTTP({
-    
+    schema: schema
 }));
 
 app.listen(4000, () => {
