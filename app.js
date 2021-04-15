e08d594d7c9129722424352416c4c357a1d8e765
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-15 09:59:34 +0100

    connect to mongoDB database using mongoose

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// connect to mongoDB database
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb+srv://Avi:0549322873@digitallibrary.iuzlk.mongodb.net/digitalLibrary?retryWrites=true&w=majority");
+mongoose.connection.once("open", () => {
+    console.log("connected to database successfully");
+});
 
 // middleware to tell express to use graphql and what schema to use
 app.use("/graphql", graphqlHTTP({
