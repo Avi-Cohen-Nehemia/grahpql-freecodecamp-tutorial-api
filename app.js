7a64498c043fa80c757cb4cb3226ef9e4d46ba9a
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-04-15 16:58:13 +0100

    allow cross-origin requests

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
+// allow cross-origin requests
+app.use(cors());
+
 // connect to mongoDB database
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
 mongoose.connect(process.env.DB_CONNECTION);
 mongoose.connection.once("open", () => {
     console.log("connected to database successfully");
